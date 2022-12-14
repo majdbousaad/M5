a1d9bb69b1e48458377b9692297426023ab8353d
majd bousaad <bousaadmajd@gmail.com>
2022-12-14 16:45:21 +0100

    just for you

---
+++ for nico.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `newsletter` (
+                              `id` int(11) NOT NULL AUTO_INCREMENT,
+                              `name` varchar(10) NOT NULL,
+                              `email` varchar(50) NOT NULL,
+                              `sprache` varchar(1) NOT NULL,
+                              PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=95 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+
