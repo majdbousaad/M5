2a4a4a29fa4c43646ddd8fc231f0de7d51ccaaa8
CodingToffee <84900141+CodingToffee@users.noreply.github.com>
2022-12-14 17:25:25 +0100

    A1.6, letzterfehler, letzteanmeldung

--- emensa/models/authentification.php
+++ emensa/models/authentification.php
@@ -13,9 +13,21 @@ function auth($passwort, $email)
 
     if ($data[0] != null) {
         $id = $data[0]['id'];
+        //update anzahlanmeldungen
         $sql = "UPDATE benutzer SET anzahlanmeldungen = anzahlanmeldungen +1 WHERE id = '$id'";
         mysqli_query($link,$sql);
+        //Update letzteanmeldung
+        $sql = "UPDATE benutzer SET letzteanmeldung = NOW() WHERE id = '$id'";
+        mysqli_query($link,$sql);
     }
+    else {
+        //update letzterfehler
+        $sql = "UPDATE benutzer SET letzterfehler = NOW() WHERE email = '$email'";
+        $result = mysqli_query($link,$sql);
+    }
+    var_dump($data);
     mysqli_close($link);
     return $data[0];
 }
+
+
