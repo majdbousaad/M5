253ab6192481b7481fc6919c64b959faa4a3933d
CodingToffee <84900141+CodingToffee@users.noreply.github.com>
2022-12-14 15:38:41 +0100

    Database erstellt, sql für neue table A1.1

---
+++ A1_1.sql
@@ -0,0 +1,16 @@
+CREATE TABLE benutzer (
+                          id int8 primary key auto_increment ,
+                          name varchar(200) not null,
+                          email varchar(100) not null unique ,
+                          passwort varchar(200) not null ,
+                          admin boolean not null default false,
+                          anzahlfehler int not null default 0,
+                          anzahlanmeldungen int not null ,
+                          letzteanmeldungen datetime,
+                          letzterfehler datetime
+)
+
+DROP TABLE benutzer
+
+    INSERT INTO benutzer (name, email, passwort, admin, anzahlanmeldungen)
+    VALUES (admin, 'admin@emensa.example', '306d0847977076b2830e2940fcc7f1125f3bb314',true, 0)
