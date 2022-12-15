860cb20af0cd8929a6faf9b6bddad232df86ddab
majd bousaad <bousaadmajd@gmail.com>
2022-12-15 18:42:37 +0100

    Pushing Monolog in case you missed install composer packages pop up

--- emensa/vendor/composer/installed.php
+++ emensa/vendor/composer/installed.php
@@ -1,15 +1,24 @@
 <?php return array(
     'root' => array(
-        'name' => 'emensa/mvc',
+        'name' => '__root__',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => 'f2ee2a7463fa3b93da089959f971b4366a863ff1',
+        'reference' => 'b14b7a797fc6e25e7b88196058b4bfc9defbbe46',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
         'dev' => true,
     ),
     'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'b14b7a797fc6e25e7b88196058b4bfc9defbbe46',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
         'eftec/bladeone' => array(
             'pretty_version' => '4.7.1',
             'version' => '4.7.1.0',
@@ -19,14 +28,29 @@
             'aliases' => array(),
             'dev_requirement' => false,
         ),
-        'emensa/mvc' => array(
-            'pretty_version' => 'dev-master',
-            'version' => 'dev-master',
-            'reference' => 'f2ee2a7463fa3b93da089959f971b4366a863ff1',
+        'monolog/monolog' => array(
+            'pretty_version' => '2.8.0',
+            'version' => '2.8.0.0',
+            'reference' => '720488632c590286b88b80e62aa3d3d551ad4a50',
             'type' => 'library',
-            'install_path' => __DIR__ . '/../../',
+            'install_path' => __DIR__ . '/../monolog/monolog',
             'aliases' => array(),
             'dev_requirement' => false,
+        ),
+        'psr/log' => array(
+            'pretty_version' => '3.0.0',
+            'version' => '3.0.0.0',
+            'reference' => 'fe5ea303b0887d5caefd3d431c3e61ad47037001',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../psr/log',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'psr/log-implementation' => array(
+            'dev_requirement' => false,
+            'provided' => array(
+                0 => '1.0.0 || 2.0.0 || 3.0.0',
+            ),
         ),
     ),
 );
